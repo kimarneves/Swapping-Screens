58c0c7921c4754cb2b0507e47c11129e0abe77ac
Kimar Arakaki Neves <kimar@kimar.eng.br>
2016-11-15 21:46:42 -0800

    Initial Commit

---
+++ Swapping Screens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swapping Screens
+//
+//  Created by Kimar Arakaki Neves on 2016-11-15.
+//  Copyright © 2016 Kimar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
